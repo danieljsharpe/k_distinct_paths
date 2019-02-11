ca2e3e8699eff5715fdaa3eeeafc2e0b225afdda
danieljsharpe <daniel.j.sharpe1995@gmail.com>
2019-02-11 19:02:30 +0000

    rate lim cut test example fully working

--- rate_lim_cut.cpp
+++ rate_lim_cut.cpp
@@ -20,35 +20,55 @@ Feb 2019
 */
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <utility>
-//#include <boost/config.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graph_traits.hpp>
+#include <boost/graph/one_bit_color_map.hpp> // quack
 #include <boost/property_map/property_map.hpp>
 #include <boost/graph/stoer_wagner_min_cut.hpp>
+#include <boost/typeof/typeof.hpp>
 
 using namespace std;
 
-typedef boost::adjacency_list<boost::vecS,boost::vecS,boost::bidirectionalS, 
+typedef boost::adjacency_list<boost::vecS,boost::vecS,boost::undirectedS, \
             boost::no_property,boost::property<boost::edge_weight_t,double>> UndirectedGraph;
+typedef boost::graph_traits<UndirectedGraph>::vertex_descriptor vertex_descriptor;
 typedef boost::property_map<UndirectedGraph,boost::edge_weight_t>::type weight_map_type;
 typedef boost::property_traits<weight_map_type>::value_type weight_type;
+std::map<UndirectedGraph::vertex_descriptor,bool> parities_tmp;
+
 //typedef pair<double *,int **> ts_info;
 typedef pair<double *, int (*)[2]> ts_info;
+
+
+
 
 struct edge_type {
     unsigned int first;
     unsigned int second;
 };
 
-ts_info read_ts_data(const int nmin, const int nts, string tsdataf) {
+/* based on the parity map, find edges which belong to the rate-limiting cut */
+void find_cut_edges () {
 
+}
+
+// need to pass ts_ens by *reference* so that it gets modified
+ts_info read_ts_data(const int nmin, const int nts, string tsdataf, \
+                     double *ts_ens) {
+
+    string line;
+    string firstcol, secondcol, thirdcol, fourthcol;
+    int ndead_ts = 0;
     double lowest_ts_en;
+
     //double ts_wts[nts] = {1.,2.,3.};
     //double *ts_wts_ptr = &ts_wts[0];
+
     double *ts_wts = new double[nts];
-    ts_wts[0] = 1.; ts_wts[1] = 2.; ts_wts[2] = 3.;
+    //ts_wts[0] = 1.; ts_wts[1] = 2.; ts_wts[2] = 3.;
     /*
     int **ts_conns = new int*[nts];
     for (int i=0;i<nts;i++) {
@@ -58,8 +78,20 @@ ts_info read_ts_data(const int nmin, const int nts, string tsdataf) {
     }
     */
     int (*ts_conns)[2] = new int[nts][2];
-    for (int i=0;i<nts;i++) {
-        ts_conns[i][0] = 5; ts_conns[i][1] = 6; }
+    //for (int i=0;i<nts;i++) {
+    //    ts_conns[i][0] = 5; ts_conns[i][1] = 6; }
+
+    int i = 0;
+    ifstream infile(tsdataf);
+    infile.clear();
+    infile.seekg(0,infile.beg);
+    while (infile >> firstcol >> secondcol >> thirdcol >> fourthcol) {
+        ts_ens[i] = stod(firstcol);
+        ts_wts[i] = stod(firstcol);
+        ts_conns[i][0] = stod(thirdcol); ts_conns[i][1] = stod(fourthcol);
+        if (ts_conns[i][0] == ts_conns[i][1]) {} // dead TS!
+        i++;
+    }
 
     return make_pair(ts_wts, ts_conns);
 }
@@ -71,7 +103,9 @@ int main(int argc, char *argv[]) {
     const int nts = stoi(argv[2]);
     string tsdataf = argv[3];
 
-    ts_info ts_info1 = read_ts_data(nmin,nts,tsdataf);
+    double *ts_ens = new double[nts];
+
+    ts_info ts_info1 = read_ts_data(nmin,nts,tsdataf,ts_ens);
 
     double *ts_wts_ptr = ts_info1.first;
     //int **ts_conns_ptr = ts_info1.second;
@@ -92,9 +126,31 @@ int main(int argc, char *argv[]) {
     UndirectedGraph ktn(ktn_edges,ktn_edges+nts,ktn_wts,nmin,nts);
 
     // run the Stoer-Wagner algorithm from the boost library
-    //BOOST_AUTO ...
+    auto parities = boost::make_assoc_property_map(parities_tmp);
+    int cut_wt = boost::stoer_wagner_min_cut(ktn,get(boost::edge_weight,ktn), \
+            boost::parity_map(parities));
+    
+    // copied from stoer-wagner example
+    cout << "The min-cut weight of G is " << cut_wt << "\n" << endl;
+
+    cout << "One set of vertices consists of:" << endl;
+    for (int i = 0; i < num_vertices(ktn); ++i) {
+        if (get(parities, i))
+            cout << i << endl;
+      }
+      cout << endl;
+      
+      cout << "The other set of vertices consists of:" << endl;
+      for (int i = 0; i < num_vertices(ktn); ++i) {
+        if (!get(parities, i))
+          cout << i << endl;
+      }
+      cout << endl;
+
+
 
     // cleanup
+    delete[] ts_ens;
     delete[] ts_wts_ptr;
     /*
     for (int i=0;i<nts;i++) {
